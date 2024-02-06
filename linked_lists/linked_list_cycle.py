d20da9330dbaef0a4dc687ff17cefd641da1d661
Evgeny Pintus <evgeny.pintus@getbyrd.com>
2024-02-06 10:45:58 +0100

    Add: Linked lists cycle

--- linked_lists/linked_list_cycle.py
+++ linked_lists/linked_list_cycle.py
@@ -8,3 +8,59 @@ connected to. Note that pos is not passed as a parameter.
 
 Return true if there is a cycle in the linked list. Otherwise, return false.
 """
+
+class ListNode(object):
+
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    @staticmethod
+    def from_list(arr, pos=-1):
+
+        result = None
+        cycle = None
+        current = 0
+        if arr is not None and len(arr) > 0:
+            node = ListNode(val=arr.pop(0))
+            result = node
+            if pos == current:
+                cycle = node
+                print(cycle.val)
+            for elem in arr:
+                node.next = ListNode(val=elem)
+                node = node.next
+
+                current += 1
+                if pos == current:
+                    cycle = node
+                    print(cycle.val)
+            if pos != -1:
+                node.next = cycle
+                print('cycling', cycle.val)
+        return result
+
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        s = set()
+        temp = head
+        while (temp):
+            if (temp in s):
+                return True
+            s.add(temp)
+            temp = temp.next
+        return False
+
+
+if __name__ == '__main__':
+    s = Solution()
+    l1 = [2, 4, 3, 7, 8]
+
+    list_1 = Solution.from_list(l1, 1)
+    cycles = s.hasCycle(list_1)
+    print(cycles)
