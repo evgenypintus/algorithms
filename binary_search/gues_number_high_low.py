bd48e6922b6d006c9e2d9a23bd925ff4e9e59f16
evgeny_pintus <evgeniy.pintus@gmail.com>
2024-02-07 22:54:17 +0100

    Add: Guess number higher lower

--- binary_search/gues_number_high_low.py
+++ binary_search/gues_number_high_low.py
@@ -13,4 +13,49 @@ You call a pre-defined API int guess(int num), which returns three possible resu
 1: Your guess is lower than the number I picked (i.e. num < pick).
 0: your guess is equal to the number I picked (i.e. num == pick).
 Return the number that I picked.
+
+The guess API is already defined for you.
+@param num, your guess
+@return -1 if num is higher than the picked number
+         1 if num is lower than the picked number
+         otherwise return 0
+def guess(num):
 """
+
+GLOBAL_NUMBER = 586889
+GLOBAL_N = 2**31 - 1
+
+def guess(number):
+
+    if number == GLOBAL_NUMBER:
+        return 0
+    elif number > GLOBAL_NUMBER:
+        return -1
+    else:
+        return 1
+
+
+def search_number(left=1, right=None):
+    """
+    :type nums: List[int]
+    :type target: int
+    :rtype: int
+    """
+    if right is None:
+        right = GLOBAL_N
+
+    mid = (left + right) // 2
+
+    print(mid)
+
+    result_guess = guess(mid)
+    if result_guess == 0:
+        return mid
+
+    if result_guess == 1:
+        return search_number(mid+1, right)
+    else:
+        return search_number(left, mid-1)
+
+
+print(search_number())
