56ed664af123137069f09f71d237c1a5cd420614
e.pintus <evgeniy.pintus@gmail.com>
2025-02-13 19:49:29 +0100

    Add: merge lists

---
+++ interview/merge_lists.py
@@ -0,0 +1,38 @@
+
+
+def merge_lists(first: list, second: list) -> list:
+    k=0
+    l=0
+    result = []
+
+    first.sort()
+    second.sort()
+
+    if len(first) == 0:
+        return second
+
+    if len(second) == 0:
+        return first
+
+    while True:
+
+        if k == len(first) and  l == len(second):
+            break
+
+        if l==len(second) or first[k] <= second[l]:
+            result.append(first[k])
+            k = k+1
+
+        else:
+            result.append(second[l])
+            l = l+1
+
+    return result
+
+
+if __name__ == '__main__':
+
+    a = [1,2,32,434]
+    b = [1]
+
+    print(merge_lists(a,b))
